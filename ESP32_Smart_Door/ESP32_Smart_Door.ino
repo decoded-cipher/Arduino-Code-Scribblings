0ad2aeeed6252c1ded6b7e35672d536b25706b6d
Arjun Krishna <arjun12345krishna@gmail.com>
2023-07-13 02:02:58 +0530

    Fetching Data from Firestore

--- ESP32_Smart_Door/ESP32_Smart_Door.ino
+++ ESP32_Smart_Door/ESP32_Smart_Door.ino
@@ -1,32 +1,68 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const char* webhook_url = "https://discord.com/api/webhooks/1128190625200738424/RhR6nM5pXIYgZcN3IVYuAqKwV9N7DUDlCqrad3S-SSmgG7RVqB-iyS-A3AyHo517eNtR";
+const char* ssid = "Cipher Den";
+const char* password = "Ak@152epv";
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.begin("Cipher Den", "Ak@152epv");
+
+
+void connectToWiFi() {
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("WiFi connected");
+  Serial.println();
+  Serial.println("Connected to WiFi...");
 }
 
-void loop() {
+
+
+String sendGetRequest(const char* path) {
   HTTPClient http;
-  http.begin(webhook_url);
-  http.addHeader("Content-Type", "application/json");
-  
-  
-  String json = "{\"content\":null,\"embeds\":[{\"title\":\":warning:  Alert! Inovus Smart Door  :warning:\",\"description\":\"Hey, did you know!\\nIt seems someone is accessing our **RFID Secured Door**!\\n\\n> Status : **Open**\\n> Tag ID : **6E 4A 2C**\\n> Tag Owner : **Arjun Krishna**\\n.\",\"color\":10381369,\"footer\":{\"text\":\"Note : Please ignore this activity if the act is legit.\"}}],\"attachments\":[]}";
-  
-  
-  int http_code = http.POST(json);
-  if (http_code == 204) {
-    Serial.println("Message sent");
+  String response = "";
+  http.begin(path);
+  int httpResponseCode = http.GET();
+  if (httpResponseCode == 200) {
+    response = http.getString();
   } else {
-    Serial.println("Error sending message");
+    Serial.print("Error - HTTP response code: ");
+    Serial.println(httpResponseCode);
   }
-  delay(10000);
+  http.end();
+  return response;
+}
+
+
+
+String processRequest(String tagId) {
+  String path = "https://firestore.googleapis.com/v1/projects/inovus-smart-door/databases/(default)/documents/tags/" + tagId;
+  Serial.println(path);
+  
+  String response = sendGetRequest(path.c_str());
+  if (response.length() > 0) {
+    Serial.println("Verified Tag ID...");
+  } else {
+    Serial.println("Invalid Tag ID...");
+  }
+  return response;
+}
+
+
+
+void setup() {
+  Serial.begin(9600);
+  connectToWiFi();
+}
+
+
+
+void loop() {
+  if (Serial.available()) {
+    String tagId = Serial.readStringUntil('\n');
+    tagId.trim();
+    Serial.println("User input: " + tagId);
+    String response = processRequest(tagId);
+  }
 }
